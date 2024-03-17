78f30a51f7b487c65ec9901a643af601c0b70b0f
Evan Zhou <evanzhoudev@gmail.com>
2024-03-16 20:02:04 -0700

    Start neo-tree automatically, don't hide dotfiles

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,33 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+	    "nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_by_name = {
+						".git",
+						".DS_Store",
+					},
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.api.nvim_create_augroup("neotree", {})
+		vim.api.nvim_create_autocmd("UiEnter", {
+			desc = "Open Neotree automatically",
+			group = "neotree",
+			callback = function()
+				if vim.fn.argc() == 0 then
+					vim.cmd("Neotree toggle")
+				end
+			end,
+		})
+	end,
 }
-

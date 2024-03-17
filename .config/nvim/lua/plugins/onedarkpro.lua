3bd339ff7ee207c1be8bb50904febc3ec935ccd4
Evan Zhou <evanzhoudev@gmail.com>
2024-03-16 20:05:49 -0700

    Enabled cursorline for One Dark Pro

--- .config/nvim/lua/plugins/onedarkpro.lua
+++ .config/nvim/lua/plugins/onedarkpro.lua
@@ -1,7 +1,12 @@
 return {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme onedark")
-    end
+	"olimorris/onedarkpro.nvim",
+	priority = 1000,
+	config = function()
+		require("onedarkpro").setup({
+			options = {
+				cursorline = true,
+			},
+		})
+		vim.cmd("colorscheme onedark")
+	end,
 }

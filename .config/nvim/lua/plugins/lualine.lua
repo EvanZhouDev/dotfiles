804a3560e8fba30079cf2576b813fe738376e67a
Evan Zhou <evanzhoudev@gmail.com>
2024-03-16 21:04:57 -0700

    Cleaned up Lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,42 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require('lualine').setup({
-        options = {
-            theme = 'onedark'
-        }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		vim.opt.showmode = false
+		require("lualine").setup({
+			options = {
+				theme = "onedark",
+				icons_enabled = true,
+				component_separators = { left = "|", right = "|" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = true,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {},
+				lualine_c = { "filename", "branch", "diagnostics" },
+				lualine_x = { "encoding", "filetype" },
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
 }

f4341063034b2a478b2fbf82e157451468b85953
Evan Zhou <evanzhoudev@gmail.com>
2024-03-22 21:26:54 -0700

    Added Python formatting

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,6 +5,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.black,
                 null_ls.builtins.diagnostics.eslint,
                 null_ls.builtins.formatting.prettier,
 			},

9ac070cb584c709494c1fdfd3a47ccc3d241a618
Evan Zhou <evanzhoudev@gmail.com>
2024-03-22 21:27:07 -0700

    Added few QOL changes to vim config

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,4 +3,10 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set signcolumn=yes")
+vim.cmd("set splitright")
+vim.cmd("noremap j gj")
+vim.cmd("nnoremap k gk")
+vim.cmd("set number")
+
 vim.g.mapleader = " "
+

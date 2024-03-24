72ccdabed41f7004a43bfacb2c8133a55fb00375
Evan Zhou <evanzhoudev@gmail.com>
2024-03-24 14:14:36 -0700

    Go back to file line based up/down movements

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -4,8 +4,6 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set signcolumn=yes")
 vim.cmd("set splitright")
-vim.cmd("noremap j gj")
-vim.cmd("nnoremap k gk")
 vim.cmd("set number")
 
 vim.g.mapleader = " "
